5f3510c68474ff6e505e0fd21c1fab17494c6ebd
wangruogu96 <31543798+wangruogu96@users.noreply.github.com>
2018-02-25 19:42:40 -0600

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,50 @@ class MoviesController < ApplicationController
   end
 
   def index
+    # default: all movies of all ratings
     @movies = Movie.all
+    @all_ratings = Movie.ratings
+    @ratings_hash = Hash[*@all_ratings.map {|key| [key, 1]}.flatten]
+    set_uri=false
+    
+    if (params[:ratings] != nil)
+      @ratings_hash = params[:ratings]
+      @movies = @movies.where(:rating => @ratings_hash.keys)
+      session[:ratings] = @ratings_hash
+    else
+      if(session[:ratings]!=nil)
+        @ratings_hash=session[:ratings]
+        @movies = @movies.where(:rating => @ratings_hash.keys)
+        set_uri=true
+      end
+    end
+    
+    @sorttype=params[:sort]
+    if(params[:sort]!=nil)
+      session[:sort]=params[:sort]
+      if(params[:sort]=="title")
+        @movies = @movies.order(:title)
+        @title = "hilite"
+      end
+      if(params[:sort]=="date")
+        @movies = @movies.order(:release_date)
+        @date = "hilite"
+      end
+    else
+      if(session[:sort] != nil)
+        params[:sort]=session[:sort]
+        set_uri=true
+        case params[:sort]
+        when "title"
+          @movies = @movies.order(:title)
+          @title = "hilite"
+        when "date"
+          @movies = @movies.order(:release_date)
+          @date = "hilite"
+        end
+      end
+    end
+    
   end
 
   def new
